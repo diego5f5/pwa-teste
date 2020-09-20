1504375cfd688f5165562f60371b7b690b02d817
Diego Ferreira <54445308+diego5f5@users.noreply.github.com>
2020-09-20 15:37:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const cacheName = "v1";
+
+const resourcesToPrecache = ["/", "index.html", "/styles.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(resourcesToPrecache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cacheResponse) => cacheResponse || fetch(event.request))
+  );
+});
